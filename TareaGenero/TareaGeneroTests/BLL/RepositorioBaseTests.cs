95f54c6102c954d9a5987f3f6f8ddbcf5473f487
leonardo emil <theflaco56@hotmail.com>
2020-01-22 09:41:31 -0400

    test

---
+++ TareaGenero/TareaGeneroTests/BLL/RepositorioBaseTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TareaGenero.BLL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TareaGenero.Entidades;
+
+namespace TareaGenero.BLL.Tests
+{
+    [TestClass()]
+    public class RepositorioBaseTests
+    {
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            RepositorioBase<Genero> db = new RepositorioBase<Genero>();
+
+
+            Genero genero = new Genero()
+            {
+                Nombre ="Drama"
+            };
+            Assert.IsTrue(db.Guardar(genero));
+        }
+        [TestMethod()]
+        public void GuardarTest2()
+        {
+            RepositorioBase<Genero> db = new RepositorioBase<Genero>();
+
+
+            Genero genero = new Genero()
+            {
+                Nombre = "Comedia"
+            };
+            Assert.IsTrue(db.Guardar(genero));
+        }
+
+    }
+}
---
+++ TareaGenero/TareaGeneroTests/Data/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TareaGeneroTests.Data
+{
+    class Contexto : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"Server=EMIL56\SQLEXPRESS; Database =Cine;Trusted_Connection =true");
+        }
+    }
+}
