7ef1160b29c96917c857b3e5577ff8a9bd84ff9c
leonardo emil <theflaco56@hotmail.com>
2020-01-22 08:59:28 -0400

    repositorioBase

---
+++ TareaGenero/Data/Contexto.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TareaGenero.Data
+{
+    public class Contexto : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"Server=EMIL56\SQLEXPRESS; Database =TestDb;Trusted_Connection =true");
+        }
+    }
+
+    public class Genero
+    {
+        [Key]
+        public int GeneroId { get; set; }
+        public string Nombre { get; set; }
+    }
+}
